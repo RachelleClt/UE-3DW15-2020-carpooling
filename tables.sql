ca04cc90b4e6d43aa0a9c8523fffb7a8aba5f941
Rachelle <34474876+RachelleClt@users.noreply.github.com>
2020-11-24 11:12:00 -0400

    Modifs liens tables

--- tables.sql
+++ tables.sql
@@ -12,11 +12,38 @@ INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `birthday`) VALUES
 (2, 'Albert', 'Dupond', 'sonemail@gmail.com', '1985-11-08'),
 (3, 'Thomas', 'Dumoulin', 'sonemail2@gmail.com', '1985-10-08');
 
+CREATE TABLE `cars` (
+  `id` int AUTO_INCREMENT NOT NULL,
+  `brand` varchar(255) NOT NULL,
+  `model` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `nbrSlots` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `cars` (`id`, `brand`, `model`, `color`, `nbrSlots`) VALUES
+(1, 'Skoda', 'Fabia', 'Noire', 5),
+(2, 'Huandai', 'Getz', 'Rouge', 5),
+(3, 'Mercedes', 'Classe C', 'Noire', 4),
+(4, 'Renaut', 'Zoé', 'Bleu', 2);
+
+CREATE TABLE users_cars (
+	user_id INT NOT NULL, 
+	car_id INT NOT NULL, 
+	PRIMARY KEY(user_id, car_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users_cars` (`user_id`, `car_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(3, 4);
+
 CREATE TABLE `annonces` (
   `id` int AUTO_INCREMENT NOT NULL,
   `titre` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
-  `date` datetime NOT NULL,
+  `jour` datetime NOT NULL,
   `depart` varchar(255) NOT NULL,
   `arrive` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
@@ -27,19 +54,19 @@ INSERT INTO `annonces` (`id`, `titre`, `lastname`, `date`, `depart`, `arrive`) V
 (2, 'Carpool depart de Troyes', 'Dupond', '2020-11-28', 'Troyes', 'Paris'),
 (3, 'Carpool depart de Reims', 'Dumoulin', '2020-11-29', 'Reims', 'Troyes');
 
-CREATE TABLE `voitures` (
-  `id` int AUTO_INCREMENT NOT NULL,
-  `marque` varchar(255) NOT NULL,
-  `modele` varchar(255) NOT NULL,
-  `couleur` varchar(255) NOT NULL,
-  `proprietaire` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+
+
+CREATE TABLE users_annonces (
+	user_id INT NOT NULL,
+	annonces_id INT NOT NULL,
+	PRIMARY KEY(user_id, annonces_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `voitures` (`id`, `marque`, `modele`, `couleur`, `proprietaire`) VALUES
-(1, 'Peugeot', '208', 'Gris', 'Godé'),
-(2, 'Peugeot', '3008', 'Noir', 'Dupond'),
-(3, 'Citroen', 'C3', 'Blanc', 'Dumoulin');
+INSERT INTO `users_annonces` (`user_id`, `annonces_id`) VALUES
+(1, 1),
+(1, 2),
+(2, 3),
+(3, 4);
 
 CREATE TABLE `reservation` (
   `id` int AUTO_INCREMENT NOT NULL,
@@ -57,7 +84,19 @@ INSERT INTO `reservation` (id, firstname, lastname, email, lieu_depart, lieu_arr
 (2, 'Vincent', 'GODE', 'vincentgode@gmail.com', 'Limoges', 'Paris', '2020-11-26'),
 (3, 'Rachelle', 'COLAT', 'rachellecolat@gmail.com', 'Paris', 'Toulouse', '2020-11-28');
 
-CREATE TABLE `commentaire` (
+CREATE TABLE users_reservation (
+	user_id INT NOT NULL,
+	reservation_id INT NOT NULL,
+	PRIMARY KEY(user_id, reservation_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users_reservation` (`user_id`, `reservation_id`) VALUES
+(1, 2),
+(1,3),
+(2, 3),
+(2, 4);
+
+CREATE TABLE `comment` (
   `id` int AUTO_INCREMENT NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `titre` varchar(255) NOT NULL,
@@ -65,7 +104,19 @@ CREATE TABLE `commentaire` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `commentaire` (`id`, `firstname`, `titre`, `commentaire`) VALUES
+INSERT INTO `comment` (`id`, `firstname`, `titre`, `commentaire`) VALUES
 (1, 'Vincent', 'Super conducteur', 'Voyage agréable,je recommande.'),
 (2, 'Paul', 'Cool', 'Chauffeur très sérieux.'),
 (3, 'Ben', 'Moyen', 'Musique trop forte, conduite trop sportive.');
+
+CREATE TABLE users_comment (
+	user_id INT NOT NULL,
+	comment_id INT NOT NULL,
+	PRIMARY KEY(user_id, comment_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `users_comment` (`user_id`, `comment_id`) VALUES
+(1, 2),
+(1,3),
+(2, 3),
+(2, 4);
